d30af50bc7a7be6a4b4602b00564f05d62f685eb
Oddurka <oddurka@gmail.com>
2019-02-06 14:46:23 +0000

    adds the functionality to send email through contact us page

--- VikingLandscaping/Models/ContactFormModel.cs
+++ VikingLandscaping/Models/ContactFormModel.cs
@@ -4,13 +4,15 @@ namespace VikingLandscaping.Models
 {
     public class ContactFormModel
     {
-        [Required]
+        [Required(ErrorMessage = "Vänligen fyll i namn")]
+        [RegularExpression(@"^([^0-9]*)$", ErrorMessage = "Ogiltigt namn")]
         public string Name { get; set; }
-        [Required]
-        public string LastName { get; set; }
-        [Required]
+        [RegularExpression(@"(\d{9})", ErrorMessage = "Måste vara ett giltigt telefonnummer")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Vänligen fyll i en e-post adress")]
+        [RegularExpression(@"^.+@[^\.].*\.[a-z]{2,}$", ErrorMessage = "Ogiltigt e-post")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Vänligen fyll i ärende")]
         public string Message { get; set; }
     }
 }
