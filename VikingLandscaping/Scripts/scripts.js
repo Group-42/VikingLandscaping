a765cb7d972ad6ae31d2c00d1a3fb37379bf3faa
EgillM <egillhalldorsson@gmail.com>
2019-02-13 14:25:01 +0000

    Fixed footer, and small mistakes

---
+++ VikingLandscaping/Scripts/scripts.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    var docHeight = $(window).height();
+    var footerHeight = $('.footer-style').height();
+    var footerTop = $('.footer-style').position().top + footerHeight;
+
+    if (footerTop < docHeight) {
+        $('.footer-style').css('margin-top', 22 + (docHeight - footerTop) + 'px');
+    }
+});
